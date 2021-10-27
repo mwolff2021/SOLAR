d025d42d11ab0d21e8338005766d8c65abbe693b
RTodd1422 <rtodd1422@gmail.com>
2021-10-27 13:07:40 -0600

    tried to add flight. got particle system to work.

--- MHD 2/Assets/Scripts/DataPlotter.cs
+++ MHD 2/Assets/Scripts/DataPlotter.cs
@@ -18,22 +18,21 @@ public class DataPlotter : MonoBehaviour
     public int columnY = 1;
     public int columnZ = 2;
     public int columnEmit = 3;
-    public GameObject solar; 
+   
+    //public GameObject solar; 
     public System.Random rnd = new System.Random(); 
 
     private int[,] colors = { { 0, 226, 255 }, { 0, 230, 255 }, { 0, 234, 255 }, { 0, 239, 255 }, { 0, 243, 255 }, { 0, 247, 255 }, { 0, 251, 255 }, { 0, 255, 255 }, { 0, 255, 245 }, { 0, 255, 234 }, { 0, 255, 224 }, { 0, 255, 213 }, { 0, 255, 203 }, { 0, 255, 192 }, { 0, 255, 181 }, { 0, 255, 169 }, { 0, 255, 158 }, { 0, 255, 146 }, { 0, 255, 135 }, { 0, 255, 123 }, { 0, 255, 110 }, { 0, 255, 97 }, { 0, 255, 84 }, { 0, 255, 70 }, { 0, 255, 56 }, { 0, 255, 40 }, { 0, 255, 23 }, { 0, 255, 0 }, { 9, 255, 0 }, { 15, 255, 0 }, { 21, 255, 0 }, { 26, 255, 0 }, { 31, 255, 0 }, { 36, 255, 0 }, { 40, 255, 0 }, { 45, 255, 0 }, { 49, 255, 0 }, { 54, 255, 0 } };
     private int colorIterator = 0;
-    private ParticleSystem particles = new ParticleSystem();
-    private ParticleSystem.Particle[] ps; 
-    //public ParticleSystemRenderer psr; 
-    
-    
+    private ParticleSystem particles;
+    private ParticleSystem.Particle[] ps;
+    //int numPoints = 406893;
+    int numPoints = 40000; 
     // Use this for initialization
     void Start()
     {
-        solar = GameObject.Find("Particle System");
-        particles = solar.GetComponent<ParticleSystem>();
-        int numPoints = 406893; 
+        particles = gameObject.GetComponent<ParticleSystem>();
+
         ps = new ParticleSystem.Particle[numPoints]; 
 
         pointList = CSVReader.Read(inputfile);
@@ -54,7 +53,8 @@ public class DataPlotter : MonoBehaviour
                 (float)colors[colorIterator, 2] / 255, 
                 (float)opacity);
             ps[i].startColor = particleColor;
-            ps[i].position = new Vector3(x, y, z); 
+            ps[i].position = new Vector3(x, y, z);
+            ps[i].size = 20.0f; 
 
         }
 
---
+++ MHD 2/Assets/Scripts/JetpackBasic.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class JetpackBasic : MonoBehaviour
+{
+    private Rigidbody body;
+    private SteamVR_TrackedController controller;
+
+    [SerializeField]
+    private float thrustMultipler = 14f;
+
+    [SerializeField]
+    private float maxVelocity = 100f;
+
+    private void OnEnable()
+    {
+        controller = GetComponent<SteamVR_TrackedController>();
+        body = transform.GetComponentInParent<Rigidbody>(); ;
+    }
+
+    private void FixedUpdate()
+    {
+        var thrust = controller.controllerState.rAxis1.x;
+
+        if (thrust > 0.1)
+        {
+            var forceVector = transform.forward * thrust * thrustMultipler;
+            body.AddForce(forceVector);
+
+            //SteamVR_Controller.Input((int)controller.controllerIndex).TriggerHapticPulse((ushort)(200f * thrust));
+        }
+    }
+}
