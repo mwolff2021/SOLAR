4c071ba05e00a49a8a658ecd9cfdfc659fa768f2
mwolff2021 <mmwolff@email.wm.edu>
2021-12-02 14:17:23 -0700

    switchy switchy

--- MHD 2/Assets/Scripts/CameraController.cs
+++ MHD 2/Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 
 [RequireComponent(typeof(Camera))]
@@ -12,10 +13,12 @@ public class CameraController : MonoBehaviour
 	public float lookSensitivity = 1; // mouse look sensitivity
 	public float dampingCoefficient = 5; // how quickly you break to a halt after you stop your input
 	public bool focusOnEnable = true; // whether or not to focus and lock cursor immediately on enable
+  
+    bool whichData = false; 
 
 	Vector3 velocity; // current velocity
 
-	static bool Focused
+    static bool Focused
 	{
 		get => Cursor.lockState == CursorLockMode.Locked;
 		set
@@ -32,13 +35,40 @@ public class CameraController : MonoBehaviour
 
 	void OnDisable() => Focused = false;
 
-	void Update()
+  
+
+    public void ChangeScript()
+    {
+        Debug.Log("change script called"); 
+       if (whichData)
+        {
+            Debug.Log("setting emissivity plotter to off"); 
+            GameObject.Find("Particle System").GetComponent<EmissivityPlotter>().enabled = false;
+            GameObject.Find("Particle System").GetComponent<DataPlotter>().enabled = true;
+            whichData = !whichData; 
+        }
+        else {
+            Debug.Log("setting emissivity plotter to on");
+            GameObject.Find("Particle System").GetComponent<DataPlotter>().enabled = false;
+            GameObject.Find("Particle System").GetComponent<EmissivityPlotter>().enabled = true;
+            whichData = !whichData;
+        }
+ 
+
+    }
+
+    void Update()
 	{
 		// Input
 		if (Focused)
 			UpdateInput();
 		else if (Input.GetMouseButtonDown(0))
 			Focused = true;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("f key pressed");
+            ChangeScript(); 
+        }
 
 		// Physics
 		velocity = Vector3.Lerp(velocity, Vector3.zero, dampingCoefficient * Time.deltaTime);
